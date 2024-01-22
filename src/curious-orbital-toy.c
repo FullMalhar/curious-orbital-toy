125a7b29acf032f7463b69b9cd6d1629d9be25dc
FullMalhar <palkar.malhar11@gmail.com>
2024-01-22 21:30:09 +1100

    Opens initial opengl window

---
+++ src/curious-orbital-toy.c
@@ -0,0 +1,46 @@
+// Curious Orbital Toy
+// Malhar Palkar
+#include <curious-orbital-toy.h>
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+// Array of orbital bodies
+body_t scene[COT_BODYCOUNT];
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main(int argc, char **argv)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* gWindow = glfwCreateWindow(800, 600, "Curious Orbital Toy", NULL, NULL);
+    if (!gWindow)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(gWindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        return -1;
+    
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(gWindow, framebuffer_size_callback); 
+
+    while(!glfwWindowShouldClose(gWindow))
+    {
+        glfwSwapBuffers(gWindow);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    return 0;
+}
