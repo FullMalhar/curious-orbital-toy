1004e56a164080142ccc5141522d4972164c3c52
FullMalhar <palkar.malhar11@gmail.com>
2024-10-13 17:21:59 +1100

    AddED CPU usage

--- src/cot-metrics.cpp
+++ src/cot-metrics.cpp
@@ -2,12 +2,17 @@
 // Malhar Palkar
 #include <curious-orbital-toy.hpp>
 
+#include <iomanip>
 #include <sstream>
 #include <sys/resource.h>
+#include <sys/times.h>
 #include <unistd.h>
 
 static sf::Font sfFntMetrics;
 static sf::Text sfTxtMetrics;
+
+static clock_t lastCPU, lastSysCPU, lastUserCPU;
+static int numProcessors;
 
 bool cot::metrics::setup()
 {
@@ -20,6 +25,20 @@ bool cot::metrics::setup()
     sfTxtMetrics.setCharacterSize(24);
     sfTxtMetrics.setFillColor(sf::Color::White);
     sfTxtMetrics.setStyle(sf::Text::Bold);
+
+    // Prepare to collect CPU usage data
+    FILE* file;
+    struct tms timeSample;
+    char line[128];
+    lastCPU = times(&timeSample);
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+    file = fopen("/proc/cpuinfo", "r");
+    numProcessors = 0;
+    while(fgets(line, 128, file) != NULL){
+        if (strncmp(line, "processor", 9) == 0) numProcessors++;
+    }
+    fclose(file);
 
     return true;
 }
@@ -52,11 +71,39 @@ void cot::metrics::update(const cot::math_t dt)
     }
     std::size_t mem = (size_t)rss * (size_t)sysconf( _SC_PAGESIZE) / (1024U * 1024U);
 
+    // Calculate CPU usage
+    static cot::math_t cpu_usage = 0.0f;
+    static std::size_t cpu_usage_delay = 11;
+    if (cpu_usage_delay > 100)
+    {
+        struct tms timeSample;
+        clock_t now;
+        now = times(&timeSample);
+        if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+            timeSample.tms_utime < lastUserCPU)
+        {
+            cpu_usage = -1.0;
+        }
+        else
+        {
+            cpu_usage = (timeSample.tms_stime - lastSysCPU) + (timeSample.tms_utime - lastUserCPU);
+            cpu_usage /= (now - lastCPU);
+            cpu_usage /= numProcessors;
+            cpu_usage *= 100;
+        }
+        lastCPU = now;
+        lastSysCPU = timeSample.tms_stime;
+        lastUserCPU = timeSample.tms_utime;
+        cpu_usage_delay = 0;
+    }
+    cpu_usage_delay++;
+
     // Generate metrics text
     std::ostringstream strMets;
-    strMets.precision(0);
-    strMets << "RAM: " << std::fixed << mem << "MB" << '\t';
-    strMets << "FPS: " << std::fixed << std::abs(fr_avg);
+    strMets.setf(std::ios::fixed);
+    strMets << "CPU: " << std::setprecision(2) << std::setw(5) << cpu_usage << "% " << '\t';
+    strMets << "RAM: " << std::setprecision(0) << mem << "MB" << '\t';
+    strMets << "FPS: " << std::setprecision(0) << std::abs(fr_avg);
     sfTxtMetrics.setString(strMets.str());
 }
 
