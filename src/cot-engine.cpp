0297723c059324a2dee75015b5ec441ee681e556
FullMalhar <palkar.malhar11@gmail.com>
2024-10-13 14:02:44 +1100

    Got RAM usage

--- src/cot-engine.cpp
+++ src/cot-engine.cpp
@@ -4,6 +4,8 @@
 
 #include <ctgmath>
 #include <sstream>
+#include <sys/resource.h>
+#include <unistd.h>
 
 /**
  * @brief Calculates the gravitational attraction force between 2 bodies
@@ -152,7 +154,7 @@ void cot::Engine::update(const cot::math_t dt)
             this->vSystem[i].planet.getRadius() * std::sin(forceVectorAngle));
     }
 
-    // Calculate framerate
+    // Calculate averaging framerate
     static const std::size_t nFr = 100;
     static cot::math_t fr[nFr];
     for (std::size_t i = nFr - 1; i > 0; i--)
@@ -167,9 +169,21 @@ void cot::Engine::update(const cot::math_t dt)
     }
     fr_avg /= static_cast<cot::math_t>(nFr);
 
+    // Calculate current RAM usage
+    static std::size_t cRam = 0;
+    long rss = 0L;
+    FILE* fp = fopen("/proc/self/statm", "r");
+    if (fp)
+    {
+        fscanf( fp, "%*s%ld", &rss );
+        fclose(fp);
+    }
+    std::size_t mem = (size_t)rss * (size_t)sysconf( _SC_PAGESIZE) / (1024U * 1024U);
+
     // Generate metrics text
     std::ostringstream strMets;
     strMets.precision(0);
+    strMets << "RAM: " << std::fixed << mem << "MB" << '\t';
     strMets << "FPS: " << std::fixed << std::abs(fr_avg);
     this->sfTxtMetrics.setString(strMets.str());
 }
